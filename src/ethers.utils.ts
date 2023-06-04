a6bfdb5efd1565bc84a37bfee868530c08bfee05
TrejGun <trejgun@gmail.com>
2023-06-04 12:02:43 +0400

    fix: monkey patch BigInt

--- src/ethers.utils.ts
+++ src/ethers.utils.ts
@@ -1,5 +1,18 @@
 import { JsonRpcProvider, Log } from "ethers";
 
+// Patch BigNumber
+// https://github.com/GoogleChromeLabs/jsbi/issues/30
+// eslint-disable-next-line no-extend-native
+Object.defineProperty(BigInt.prototype, "toJSON", {
+  value: function () {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return this.toString();
+  },
+  configurable: true,
+  enumerable: false,
+  writable: true,
+});
+
 export const getPastEvents = async (
   provider: JsonRpcProvider,
   address: Array<string>,
