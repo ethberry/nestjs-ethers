71164b7fcb4ad985a071303322ed07c9ee00532e
Yury Toporov <gemuniongroup@gmail.com>
2023-09-08 21:20:16 +0800

    fix: imports

--- modules/ethers/src/ethers.contract.module.ts
+++ modules/ethers/src/ethers.contract.module.ts
@@ -2,7 +2,6 @@ import { DynamicModule, Logger, Module, OnModuleDestroy, OnModuleInit } from "@n
 import { ConfigModule } from "@nestjs/config";
 import { ScheduleModule } from "@nestjs/schedule";
 import { createConfigurableDynamicRootModule } from "@golevelup/nestjs-modules";
-import { DiscoveryModule } from "@golevelup/nestjs-discovery";
 
 import { LicenseModule, licenseProvider } from "@gemunion/nest-js-module-license";
 
@@ -12,7 +11,7 @@ import { IModuleOptions } from "./interfaces";
 import { MODULE_OPTIONS_PROVIDER } from "./ethers.constants";
 
 @Module({
-  imports: [ConfigModule, DiscoveryModule, ScheduleModule.forRoot(), LicenseModule.deferred()],
+  imports: [ConfigModule, ScheduleModule.forRoot(), LicenseModule.deferred()],
   providers: [ethersRpcProvider, licenseProvider, Logger, EthersContractService, redisQueueProvider],
   exports: [EthersContractService],
 })
