5c6864cace12aa25d85c3dd6ab465084f99900e1
TrejGun <trejgun@gmail.com>
2024-09-28 18:53:48 +0400

    fix: do not export ethersSignerProvider

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,2 +1,2 @@
 export * from "./rpc";
-export * from "./signer";
+// export * from "./signer";
--- src/providers/signer.ts
+++ src/providers/signer.ts
@@ -3,6 +3,11 @@ import { JsonRpcProvider, Wallet } from "ethers";
 
 import { ETHERS_RPC, ETHERS_SIGNER } from "../ethers.constants";
 
+// This is just an example for testing purposes.
+// Do not store private keys in public configuration.
+// Use a secure storage solution like
+// AWS Secrets Manager or Google Cloud Secret Manager
+// to store and retrieve sensitive information securely.
 export const ethersSignerProvider = {
   provide: ETHERS_SIGNER,
   inject: [ConfigService, ETHERS_RPC],
