aac32cab0c7a12106cf885e0ee86d417e86da813
TrejGun <trejgun@gmail.com>
2024-09-29 17:08:39 +0400

    fix: recursivelyDecodeResult

--- src/utils/decode-result.spec.ts
+++ src/utils/decode-result.spec.ts
@@ -1,11 +1,12 @@
 import { Interface } from "ethers";
 
-import { abi } from "../contracts/ERC721Ownable.json";
+import erc721ABI from "../contracts/ERC721Ownable.json";
+import exchangeABI from "../contracts/Exchange.json";
 import { recursivelyDecodeResult } from "./decode-result";
 
-describe("Ethers", function() {
-  it("parseLog", function() {
-    const iface = new Interface(abi);
+describe("Ethers", function () {
+  it("parseLog 1", function () {
+    const iface = new Interface(erc721ABI.abi);
     const logDescription = iface.parseLog({
       data: "0x",
       topics: [
@@ -22,6 +23,31 @@ describe("Ethers", function() {
       tokenId: 4n,
       approved: "0xd6A7c915066E17ba18024c799258C8A286fFBc00",
       owner: "0xFE3B557E8Fb62b89F4916B721be55cEb828dBd73",
+    };
+
+    expect(actual).toEqual(expected);
+  });
+
+  it("parseLog 2", function () {
+    const iface = new Interface(exchangeABI.abi);
+    const logDescription = iface.parseLog({
+      data: "0x000000000000000000000000fe3b557e8fb62b89f4916b721be55ceb828dbd73000000000000000000000000e03ef2490316bff9808d936eee70f23896f075480000000000000000000000000000000000000000000000000000000000000001000000000000000000000000fe3b557e8fb62b89f4916b721be55ceb828dbd730000000000000000000000002114de86c8ea1fd8144c2f1e1e94c74e498afb1b0000000000000000000000000000000000000000000000000000000000989680",
+      topics: ["0xf3b9076b481e6f90328250079267bf3928dcec4938f909ef39f283615432c0f1"],
+    });
+
+    const actual = recursivelyDecodeResult(logDescription!.args);
+
+    const expected = {
+      item: {
+        account: "0xFE3B557E8Fb62b89F4916B721be55cEb828dBd73",
+        token: "0xE03Ef2490316bfF9808d936eEe70f23896F07548",
+        tokenId: 1n,
+      },
+      price: {
+        account: "0xFE3B557E8Fb62b89F4916B721be55cEb828dBd73",
+        token: "0x2114De86c8Ea1FD8144C2f1e1e94C74E498afB1b",
+        amount: 10000000n,
+      },
     };
 
     expect(actual).toEqual(expected);
--- src/utils/decode-result.ts
+++ src/utils/decode-result.ts
@@ -2,20 +2,21 @@ import { Result } from "ethers";
 
 export const recursivelyDecodeResult = (result: Result): Record<string, any> => {
   if (typeof result !== "object") {
-    // Raw primitive
+    // Raw primitive value
     return result;
   }
-
-  const obj = result.toObject();
-  if (obj._) {
-    // Array
-    return result.toArray().map(item => recursivelyDecodeResult(item));
+  try {
+    const obj = result.toObject();
+    if (obj._) {
+      throw new Error("Decode as array, not object");
+    }
+    Object.keys(obj).forEach(key => {
+      obj[key] = recursivelyDecodeResult(obj[key]);
+    });
+    return obj;
+  } catch (e) {
+    // Result is array.
+    void e;
+    return result.toArray().map(item => recursivelyDecodeResult(item as Result));
   }
-
-  Object.keys(obj).forEach(key => {
-    // Object
-    obj[key] = recursivelyDecodeResult(obj[key]);
-  });
-
-  return obj;
 };
